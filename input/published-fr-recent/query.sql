6402579c4221125cc00a02bf6f35aa7c0bb685c4
Bruno Perel <brunoperel@gmail.com>
2022-03-09 20:10:00 +0100

    Improve queries and scrape process

--- input/published-fr-recent/query.sql
+++ input/published-fr-recent/query.sql
@@ -1,13 +1,13 @@
-CREATE OR REPLACE VIEW duckguessr_published_fr_recent_game AS
+CREATE OR REPLACE VIEW duckguessr_published_fr_recent_game_all AS
 SELECT DISTINCT entryurl.id AS entryurl_id,
                 storyjob.personcode
 FROM inducks_issue issue
-         INNER JOIN inducks_entry entry ON issue.issuecode = entry.issuecode
-         INNER JOIN inducks_entryurl entryurl ON entry.entrycode = entryurl.entrycode
-         INNER JOIN inducks_storyversion storyversion
-                    ON entry.storyversioncode = storyversion.storyversioncode
-         INNER JOIN inducks_story story ON storyversion.storycode = story.storycode
-         INNER JOIN inducks_storyjob storyjob ON storyversion.storyversioncode = storyjob.storyversioncode
+     INNER JOIN inducks_entry entry ON issue.issuecode = entry.issuecode
+     INNER JOIN inducks_entryurl entryurl ON entry.entrycode = entryurl.entrycode
+     INNER JOIN inducks_storyversion storyversion
+                ON entry.storyversioncode = storyversion.storyversioncode
+     INNER JOIN inducks_story story ON storyversion.storycode = story.storycode
+     INNER JOIN inducks_storyjob storyjob ON storyversion.storyversioncode = storyjob.storyversioncode
 WHERE issue.publicationcode IN ('fr/MP', 'fr/PM', 'fr/SPG')
   AND oldestdate > '2010-00-00'
   AND sitecode = 'thumbnails3'
@@ -15,23 +15,30 @@ WHERE issue.publicationcode IN ('fr/MP', 'fr/PM', 'fr/SPG')
   AND plotwritartink = 'a'
   AND personcode <> '?';
 
-SELECT DISTINCT @dataset_id,
-                GROUP_CONCAT(CONCAT(sitecode, '/', url) ORDER BY entryurl.id DESC SEPARATOR '|') AS entryurl_urls,
-                storyjob.personcode
-FROM duckguessr_published_fr_recent_game entryurl_ids
-         INNER JOIN inducks_entryurl entryurl ON entryurl_ids.entryurl_id = entryurl.id
-         INNER JOIN
-     (SELECT artist_with_20_entries.personcode, nationalitycountrycode, fullname
-      FROM duckguessr_published_fr_recent_game artist_with_20_entries
-               INNER JOIN inducks_person person ON artist_with_20_entries.personcode = person.personcode
-      GROUP BY artist_with_20_entries.personcode
+CREATE OR REPLACE VIEW duckguessr_published_fr_recent_game AS
+SELECT storyjob.personcode,
+       sitecode,
+       url,
+       entryurl_id
+FROM duckguessr_published_fr_recent_game_all entryurl_ids
+     INNER JOIN inducks_entryurl entryurl ON entryurl_ids.entryurl_id = entryurl.id
+     INNER JOIN inducks_entry entry USING (entrycode)
+     INNER JOIN inducks_storyversion storyversion USING (storyversioncode)
+     INNER JOIN inducks_story story ON storyversion.storycode = story.storycode
+     INNER JOIN inducks_storyjob storyjob USING (storyversioncode)
+     INNER JOIN
+     (SELECT artist_with_2010s_entries.personcode, nationalitycountrycode, fullname
+      FROM duckguessr_published_fr_recent_game_all artist_with_2010s_entries
+           INNER JOIN inducks_person person ON artist_with_2010s_entries.personcode = person.personcode
+      GROUP BY artist_with_2010s_entries.personcode
       HAVING COUNT(*) >= 20
-     ) AS person ON person.personcode = entryurl_ids.personcode
-         INNER JOIN inducks_entry entry ON entryurl.entrycode = entry.entrycode
-         INNER JOIN inducks_storyversion storyversion
-                    ON entry.storyversioncode = storyversion.storyversioncode
-         INNER JOIN inducks_story story ON storyversion.storycode = story.storycode
-         INNER JOIN inducks_storyjob storyjob ON storyversion.storyversioncode = storyjob.storyversioncode
+     ) AS artist_with_at_least_20_entries ON artist_with_at_least_20_entries.personcode = storyjob.personcode;
+
+CREATE OR REPLACE VIEW duckguessr_published_fr_recent_artists AS
+SELECT DISTINCT personcode FROM duckguessr_published_fr_recent_game;
+
+SELECT personcode,
+       GROUP_CONCAT(DISTINCT CONCAT(sitecode, '/', url) ORDER BY entryurl_id DESC SEPARATOR '|') AS entryurl_urls
+FROM duckguessr_published_fr_recent_game
 GROUP BY personcode;
 
-DROP VIEW duckguessr_published_fr_recent_game;
--- scrape.js
+++ scrape.js
@@ -23,11 +23,15 @@ for (const datasetSuffix of ['', '-ml']) {
 // const imageRoot="https://inducks.org/hr.php?normalsize=1&image=https://outducks.org/"
 const imageRoot = 'https://res.cloudinary.com/dl7hskxab/image/upload/inducks-covers/'
 
+const filePathRoot = 'input/full/'
 
 const addUrlToDataset = (dgConnection, personcode, url, datasetId) => {
-  const filePath = `input/full/${url}`;
-  return new Promise((resolve) => {
+  const filePath = `${filePathRoot}${url}`;
+  return new Promise((resolve, reject) => {
     magic.detectFile(filePath, async (err, result) => {
+      if (url === 'thumbnails3/webusers/2018/05/fr_mp_0345p174_001.jpg') {
+        debugger
+      }
       if (/^image\//.test(result)) {
         await dgConnection.query(
           "insert ignore into dataset_entryurl(dataset_id, sitecode_url) VALUES(?, ?)",
@@ -37,11 +41,21 @@ const addUrlToDataset = (dgConnection, personcode, url, datasetId) => {
           "insert ignore into entryurl_details(sitecode_url, personcode, decision) VALUES(?, ?, ?)",
           [url, personcode, null]
         )
-      } else {
-        console.log(`Marking ${filePath} as invalid`)
+
+        // Update the entryurl's personcode, in case a previous scrape stored it wrong
         await dgConnection.query(
-          "delete from dataset_entryurl where dataset_id = ? and sitecode_url = ?",
+          "update entryurl_details set personcode=? where sitecode_url = ?",
+          [personcode, url]
+        )
+      } else {
+        console.log(`Marking ${filePath.replace(filePathRoot, '')} as invalid`)
+        await dgConnection.query(
+          "delete from dataset_entryurl where sitecode_url = ?",
           [datasetId, url]
+        )
+        await dgConnection.query(
+          "delete from entryurl_details where sitecode_url = ?",
+          [url]
         )
         await dgConnection.query(
           "insert ignore into entryurl_details(sitecode_url, personcode, decision) VALUES(?, ?, ?)",
@@ -50,6 +64,7 @@ const addUrlToDataset = (dgConnection, personcode, url, datasetId) => {
         if (fs.existsSync(filePath)) {
           fs.unlinkSync(filePath)
         }
+        reject('Invalid')
       }
       resolve('OK')
     })
@@ -58,9 +73,9 @@ const addUrlToDataset = (dgConnection, personcode, url, datasetId) => {
 
 const downloadAndAddUrlToDataset = (dgConnection, personcode, url, datasetId) => {
   return new Promise(async (resolve, reject) => {
-    const filePath = `input/full/${url}`;
+    const filePath = `${filePathRoot}${url}`;
     const isEntryurlInvalid = (await dgConnection.query(
-      "select decision from entryurl_details where decision <> 'ok' and sitecode_url=?",
+      "select decision from entryurl_details where (decision is not null and decision <> 'ok') and sitecode_url=?",
       [url]
     )).length > 0;
     if (isEntryurlInvalid) {
@@ -82,10 +97,15 @@ const downloadAndAddUrlToDataset = (dgConnection, personcode, url, datasetId) =>
       const file = fs.createWriteStream(filePath);
       await https.get(`${imageRoot}/${url}`, async response => {
         response.pipe(file);
-        addUrlToDataset(dgConnection, personcode, url, datasetId).then(() => {
-          console.log(`Downloaded ${url}`)
-        })
-        resolve('OK');
+        addUrlToDataset(dgConnection, personcode, url, datasetId)
+          .then(() => {
+            console.log(`Downloaded ${url}`)
+          })
+          .catch(() => {
+          })
+          .finally(() => {
+            resolve('OK');
+          })
       }).on('error', () => {
         reject(`Skipped ${url} (could not download)`)
       });
@@ -95,7 +115,8 @@ const downloadAndAddUrlToDataset = (dgConnection, personcode, url, datasetId) =>
 
 const downloadDatasetFromQuery = (coaConnection, dgConnection, datasetSuffix, datasetId) => {
   return new Promise(async (resolve) => {
-    const [, data] = await coaConnection.query(fs.readFileSync(`input/${baseDatasetName}/query${datasetSuffix}.sql`).toString());
+    const response = (await coaConnection.query(fs.readFileSync(`input/${baseDatasetName}/query${datasetSuffix}.sql`).toString()));
+    const data = response[0].personcode ? response : response[response.length-1]
     for (const {personcode, entryurl_urls} of data) {
       const urls = entryurl_urls.split('|')
       for (const url of urls) {
